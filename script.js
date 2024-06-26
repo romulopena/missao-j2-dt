edee2e03dd9db965432c0b3b7c7932a4c67da189
PROF ROMULO <rjlpena@gmail.com>
2024-06-26 16:50:53 -0300

    REUNIÃO 4

---
+++ script.js
@@ -0,0 +1,26 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta1",
+        alternativas: [
+            "alternativa1", "alternativa2"
+        ]
+    },
+    {
+        enunciado: "Pergunta2",
+        alternativas: [
+            "alternativa1", "alternativa2"
+        ]
+    },
+    {
+        enunciado: "Pergunta3",
+        alternativas: [
+            "alternativa1", "alternativa2"
+        ]
+    }
+];
